8389d8b22e41cd18d464bd599975db0323e2ee2f
leonardo munoz 🌑🌍 <168220131+leonardomunooz@users.noreply.github.com>
2024-06-11 20:06:42 +0000

    se ha agregado la funcion showArrayList que  muestra un arreglo

--- app.js
+++ app.js
@@ -2,27 +2,30 @@
 // let adj = ["great", "big", "small", "clean", "dirty", "ugly", "funny"];
 // let noun = ["jogger", "racoon", "book", "store", "movie"];
 
-let pronoun = ['the', 'our'];
-let adj = ['great', 'big'];
-let noun = ['jogger', 'racoon'];
-let extentions = ['.com','.net','.us','.io','.ve'];
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extentions = [".com", ".net", ".us", ".io", ".ve"];
 let domainGeneratorArray = [];
 
-function domainGeneratorList (pronoun1,adj1,noun1){
-    for (let index = 0; index < pronoun1.length; index++) {
-        for (let adjItem = 0; adjItem < adj1.length; adjItem++) {
-          for (let nounItem = 0; nounItem < noun1.length; nounItem++) {
-            //    console.log(`${pronoun[index]}${adj[adjItem]}${noun[nounItem]}.com`);;
-             let domianItemGenerated = pronoun1[index] + adj1[adjItem] + noun1[nounItem];
-             domainGeneratorArray.push(domianItemGenerated);
-          }
-        }
+function domainGeneratorList(pronoun1, adj1, noun1) {
+  for (let index = 0; index < pronoun1.length; index++) {
+    for (let adjItem = 0; adjItem < adj1.length; adjItem++) {
+      for (let nounItem = 0; nounItem < noun1.length; nounItem++) {
+        //    console.log(`${pronoun[index]}${adj[adjItem]}${noun[nounItem]}.com`);;
+        let domianItemGenerated =
+          pronoun1[index] + adj1[adjItem] + noun1[nounItem];
+        domainGeneratorArray.push(domianItemGenerated);
       }
-      return domainGeneratorArray;
+    }
+  }
+  return domainGeneratorArray;
 }
 
-// console.log(typeof domainGeneratorArray);
-console.log(domainGeneratorList(pronoun,adj,noun))
+function showArrayList(arrayList) {
+  arrayList.forEach((Element) => {
+    console.log(Element + ".com");
+  });
+}
 
-
-
+showArrayList(domainGeneratorList(pronoun, adj, noun));
